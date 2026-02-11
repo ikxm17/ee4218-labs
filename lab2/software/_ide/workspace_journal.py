17138c58157822134e912471970ce5ff2bc9cfd6
leeey03 <hannah.leeey03@gmail.com>
2026-02-11 17:07:41 +0800

    add board design and hello world example for lab2

--- lab2/software/_ide/workspace_journal.py
+++ lab2/software/_ide/workspace_journal.py
@@ -4,3 +4,15 @@ import vitis
 client = vitis.create_client()
 client.set_workspace(path="software")
 
+platform = client.create_platform_component(name = "platform_matmul",hw_design = "$COMPONENT_LOCATION/../../hardware/hardware.platforms/design_1_wrapper.xsa",os = "standalone",cpu = "psu_cortexa53_0",domain_name = "standalone_psu_cortexa53_0",architecture = "64-bit",compiler = "gcc")
+
+platform = client.get_component(name="platform_matmul")
+status = platform.build()
+
+comp = client.get_component(name="xuartps_hello_world_example")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
