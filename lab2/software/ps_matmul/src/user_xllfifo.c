3ba52cd97e048304cc10cfc85aca6fa0ebca7c7f
Irwin <irwin.kxm@outlook.com>
2026-02-21 16:42:35 +0800

    autoformat

--- lab2/software/ps_matmul/src/user_xllfifo.c
+++ lab2/software/ps_matmul/src/user_xllfifo.c
@@ -13,18 +13,18 @@
 
 /* Init Function for AXI FIFO Instance */
 #ifndef SDT
-    int AXI_Init(XLlFifo *AxiFifo, u8 DeviceId)
+int AXI_Init(XLlFifo* AxiFifo, u8 DeviceId)
 #else
-    int AXI_Init(XLlFifo *AxiFifo, u32 BaseAddress)
+int AXI_Init(XLlFifo* AxiFifo, u32 BaseAddress)
 #endif
 {
-    int Status = XST_SUCCESS;
-    XLlFifo_Config *Config;
-	#ifndef SDT
+	int             Status = XST_SUCCESS;
+	XLlFifo_Config* Config;
+#ifndef SDT
 	Config = XLlFfio_LookupConfig(DeviceId);
-	#else
+#else
 	Config = XLlFfio_LookupConfig(BaseAddress);
-	#endif
+#endif
 
 	Status = XLlFifo_CfgInitialize(AxiFifo, Config, Config->BaseAddress);
 	if (Status != XST_SUCCESS) {
@@ -34,62 +34,66 @@
 	}
 
 	Status = XLlFifo_Status(AxiFifo);
-	XLlFifo_IntClear(AxiFifo,0xffffffff);
+	XLlFifo_IntClear(AxiFifo, 0xffffffff);
 	Status = XLlFifo_Status(AxiFifo);
-	if(Status != 0x0) {
-		xil_printf("\n ERROR : Reset value of ISR0 : 0x%x\t"
-			    "Expected : 0x0\n\r",
-			    XLlFifo_Status(AxiFifo));
+	if (Status != 0x0) {
+		xil_printf(
+			"\n ERROR : Reset value of ISR0 : 0x%x\t"
+			"Expected : 0x0\n\r",
+			XLlFifo_Status(AxiFifo)
+		);
 		return XST_FAILURE;
 	}
-    return Status;
+	return Status;
 }
+
 /**
  * @brief
  *
  * @param TxConfig
  * @return uint8_t
  */
-uint8_t XLlFifo_TxSend(XLlFifo_TxConfig *TxConfig)
+uint8_t XLlFifo_TxSend(XLlFifo_TxConfig* TxConfig)
 {
-    u32 TotalWords = TxConfig->tx_params->number_of_packets * TxConfig->tx_params->max_packet_length;
+	u32 TotalWords = TxConfig->tx_params->number_of_packets * TxConfig->tx_params->max_packet_length;
 
-    for (size_t i = 0; i < TotalWords; i++)
-    {
-        // Wait until there is at least one slot available
-        while (XLlFifo_iTxVacancy(TxConfig->instance_ptr) == 0);
-        
-        XLlFifo_TxPutWord(TxConfig->instance_ptr, *(TxConfig->source_addr + i));
-    }
+	for (size_t i = 0; i < TotalWords; i++) {
+		// Wait until there is at least one slot available
+		while (XLlFifo_iTxVacancy(TxConfig->instance_ptr) == 0)
+			;
 
-    // TLR write must be in BYTES (Words * 4)
-    XLlFifo_iTxSetLen(TxConfig->instance_ptr, TxConfig->tx_params->transmission_length);
+		XLlFifo_TxPutWord(TxConfig->instance_ptr, *(TxConfig->source_addr + i));
+	}
 
-    while (!(XLlFifo_IsTxDone(TxConfig->instance_ptr)));
+	// TLR write must be in BYTES (Words * 4)
+	XLlFifo_iTxSetLen(TxConfig->instance_ptr, TxConfig->tx_params->transmission_length);
 
-    return XST_SUCCESS;
+	while (!(XLlFifo_IsTxDone(TxConfig->instance_ptr)))
+		;
+
+	return XST_SUCCESS;
 }
 
 /**
- * @brief 
- * 
- * @param RxConfig 
- * @return uint8_t 
+ * @brief
+ *
+ * @param RxConfig
+ * @return uint8_t
  */
-uint8_t XLlFifo_RxReceive(XLlFifo_RxConfig *RxConfig)
+uint8_t XLlFifo_RxReceive(XLlFifo_RxConfig* RxConfig)
 {
-    // 1. Wait for the FIFO to report that at least one packet is available
-    while (XLlFifo_iRxOccupancy(RxConfig->instance_ptr) == 0);
+	// 1. Wait for the FIFO to report that at least one packet is available
+	while (XLlFifo_iRxOccupancy(RxConfig->instance_ptr) == 0)
+		;
 
-    // 2. Read the length register ONCE to "unlock" the packet
-    u32 BytesToRead = XLlFifo_iRxGetLen(RxConfig->instance_ptr);
-    u32 WordsToRead = BytesToRead / 4; 
+	// 2. Read the length register ONCE to "unlock" the packet
+	u32 BytesToRead = XLlFifo_iRxGetLen(RxConfig->instance_ptr);
+	u32 WordsToRead = BytesToRead / 4;
 
-    // 3. Read the words
-    for (size_t i = 0; i < WordsToRead; i++)
-    {
-        *(RxConfig->destination_addr + i) = XLlFifo_RxGetWord(RxConfig->instance_ptr);
-    }
+	// 3. Read the words
+	for (size_t i = 0; i < WordsToRead; i++) {
+		*(RxConfig->destination_addr + i) = XLlFifo_RxGetWord(RxConfig->instance_ptr);
+	}
 
-    return XLlFifo_IsRxDone(RxConfig->instance_ptr) ? XST_SUCCESS : XST_FAILURE;
+	return XLlFifo_IsRxDone(RxConfig->instance_ptr) ? XST_SUCCESS : XST_FAILURE;
 }
--- lab2/software/ps_matmul/src/user_xllfifo.h
+++ lab2/software/ps_matmul/src/user_xllfifo.h
@@ -1,12 +1,12 @@
 /**
  * @file user_xllfifo.h
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2026-02-13
- * 
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
  */
 
 #ifndef __XLLFIFO_H__
@@ -17,37 +17,41 @@ extern "C" {
 #endif
 /*
  * INCLUDES
-*/
-#include "xllfifo.h"
+ */
 #include <stdint.h>
+#include "xllfifo.h"
 
-typedef struct {
-    uint32_t word_size;
-    uint32_t number_of_packets;
-    uint32_t max_packet_length;
-    uint32_t transmission_length;
+typedef struct
+{
+	uint32_t word_size;
+	uint32_t number_of_packets;
+	uint32_t max_packet_length;
+	uint32_t transmission_length;
 } XLlFifo_TxParams;
 
-typedef struct {
-    XLlFifo* instance_ptr;
-    uint32_t* source_addr;
-    XLlFifo_TxParams* tx_params;
+typedef struct
+{
+	XLlFifo*          instance_ptr;
+	uint32_t*         source_addr;
+	XLlFifo_TxParams* tx_params;
 } XLlFifo_TxConfig;
 
-typedef struct {
-    uint32_t word_size;
+typedef struct
+{
+	uint32_t word_size;
 } XLlFifo_RxParams;
 
-typedef struct {
-    XLlFifo* instance_ptr;
-    uint32_t* destination_addr;
-    XLlFifo_RxParams* rx_params;
+typedef struct
+{
+	XLlFifo*          instance_ptr;
+	uint32_t*         destination_addr;
+	XLlFifo_RxParams* rx_params;
 } XLlFifo_RxConfig;
 
 #ifndef SDT
-    int AXI_Init(XLlFifo *AxiFifo, u8 DeviceId);
+int AXI_Init(XLlFifo* AxiFifo, u8 DeviceId);
 #else
-    int AXI_Init(XLlFifo *AxiFifo, u32 BaseAddress);
+int AXI_Init(XLlFifo* AxiFifo, u32 BaseAddress);
 #endif
 uint8_t XLlFifo_TxSend(XLlFifo_TxConfig* TxConfig);
 uint8_t XLlFifo_RxReceive(XLlFifo_RxConfig* RxConfig);
